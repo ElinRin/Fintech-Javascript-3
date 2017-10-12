3e9b4e548094d7ce68681cefa1245a8cee5ea34b
elinrin <elin.rinnel@gmail.com>
2017-10-13 00:11:34 +0300

    tests are uncommented

--- 03/5/task.js
+++ 03/5/task.js
@@ -9,18 +9,17 @@
 
 
 class romanNumerals {
-  romanToArabic = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000
-  };
-
   constructor(number) {
     this.number = number;
+    this.romanToArabic = {
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000
+    };
   }
 
   getArabicNumber(parseNumber = this.number, result = 0) {
