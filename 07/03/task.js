22966383a235470426269ca1d101c038d0819c54
ElinRin <elin.rinnel@gmail.com>
2017-11-10 00:10:13 +0300

    fix

--- 07/03/task.js
+++ 07/03/task.js
@@ -2,7 +2,7 @@ function throttle(time, callback) {
   let timer = 0;
 
   return () => {
-    if (Date.now() - timer > time) {
+    if (Date.now() >= time + timer) {
       callback();
       timer = Date.now();
     }
